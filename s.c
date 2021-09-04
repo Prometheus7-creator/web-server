47591ae1819abca45f0b8e798eee31860d78d3f5
Ansh Grover <56271149+Prometheus7-creator@users.noreply.github.com>
2021-09-05 01:19:11 +0530

    Update s.c

--- s.c
+++ s.c
@@ -1,55 +1,69 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define PORT 8080
 
 int main (int argc, char *argv[]) {
 	
 	struct sockaddr_in server;
+	int serverAddressLength = sizeof(server);
+	int socketDescriptor = socket (AF_INET, SOCK_STREAM, 0); //Creating socket
+	int requestHandlingSocket;
 	
-	//Creating socket
-	int socketDescriptor = socket (AF_INET, SOCK_STREAM, 0);
+	char *message = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 28\n\nGreetings from Ansh's server", serv_response[30000] = {0};
 	
-	char *message, serv_response[2000];
+	long socketRead;
 
 	if (socketDescriptor == -1) {
 		printf ("Failed to create socket!\n");
 		return  0;
 	}
 	
-	server.sin_addr.s_addr = inet_addr("142.251.33.110");
+	server.sin_addr.s_addr = INADDR_ANY;
 	server.sin_family = AF_INET;
-	server.sin_port = htons(80);
+	server.sin_port = htons( PORT );
+	
+	memset (server.sin_zero, '\0', sizeof(server.sin_zero));
 	
 	//Connecting to remote server
-	if (connect(socketDescriptor, (struct sockaddr *)&server, sizeof(server)) < 0) {
-		printf("Error in connection!\n");
+	if (bind(socketDescriptor, (struct sockaddr *)&server, sizeof(server)) < 0) {
+		printf("Error in bind!\n");
 		return 1;
 	}
 	
-	printf("Connected\n");
 	
-	message = "GET / HTTP/1.1\r\n\r\n";
-	
-	if (send(socketDescriptor, message, strlen(message), 0) < 0) {
-		printf("Send failed!\n");
+	if (listen(socketDescriptor, 5) < 0) {
+		printf("Listen failed!\n");
 		return 1;
 	}
 	
-	printf("Data Sent\n");
+	printf("Server listening\n");
 	
-	if (recv(socketDescriptor, serv_response, 2000, 0) < 0) {
-		printf("Receive failed!\n");
+	
+	while(1) {
+		
+		printf("Waiting for new connection\n");
+		
+		if ((requestHandlingSocket = accept(socketDescriptor, (struct sockaddr *)&server, (socklen_t*)&serverAddressLength)) < 0) {
+			printf("Accept failed!\n");
+		}
+		
+		socketRead = read(requestHandlingSocket, serv_response, 30000);
+		printf("%s\n", serv_response);
+	
+		write(requestHandlingSocket, message, strlen(message));
+	
+		printf("Greeting sent!\n");
+		//Close the socket
+		close (requestHandlingSocket);
+		
 	}
 	
-	printf("Response received\n");
-	printf("%s\n", serv_response);
-	
-	//Close the socket
 	close (socketDescriptor);
-	
-	printf("Socket closed.\n");
 	
 	return 0;
 }
